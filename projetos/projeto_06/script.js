ed4d21023278e78a314205bd1292692a0b7a083c
HELOISA VITÓRIA DOS SANTOS BATISTA <heloisa_batista@estudante.sesisenai.org.br>
2026-08-19 16:20:41 -0300

    teste

--- projetos/projeto_06/script.js
+++ projetos/projeto_06/script.js
@@ -21,6 +21,28 @@ function adicionarTarefa() {
         contadorId++;
         tarefas.push({id: contadorId, tarefa: texto, concluindo: false});
         inputText.value = "";
-        console.log(tarefas);
+        rendenizar();
+        console.log(tarefas)
     }
+}
+
+function rendenizar() {
+    lista.innerHTML = "";
+    tarefas.forEach(function(item){
+        let li = document.createElement("li");
+        li.textContent = item.tarefa
+        lista.appendChild(li);
+
+        let btnExcluir = document.createElement("button");
+        btnExcluir.innerText = "❌"
+        li.appendChild(btnExcluir);
+        btnExcluir.addEventListener("click", function(e) {
+            e.stopPropagation();
+
+            tarefas = tarefas.filter(function(t) {
+                return t.id !== item.id
+            })
+            rendenizar();
+        })
+    });
 }
